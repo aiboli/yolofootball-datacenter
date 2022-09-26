17a8db634ccff8793cf7e96b8e522541ab4959d6
Aibo Li <aibli@microsoft.com>
2022-09-26 00:46:40 -0700

    update order and user system

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var JobsManager = require('./jobs/cron-jobs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
 var actionsRouter = require('./routes/actions');
 var orderRouter = require('./routes/order');
 
@@ -39,7 +39,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 app.use('/actions', actionsRouter);
 app.use('/order', orderRouter);
 
