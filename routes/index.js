0ffdf17a3a8b403e0afc97570c9d4f5c88c354e1
aiboli <albertlabsez@gmail.com>
2022-10-08 17:13:37 -0700

    update date

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Yolofootball datacenter', game: JSON.stringify(global.testgame), fixtures: JSON.stringify(global.testfixtures), orders: JSON.stringify(global.testOrder), date: global.todayDates });
+  res.render('index', { title: 'Yolofootball datacenter', game: JSON.stringify(global.testgame), fixtures: JSON.stringify(global.testfixtures), orders: JSON.stringify(global.testOrder), date: global.todayDate });
 });
 
 module.exports = router;
